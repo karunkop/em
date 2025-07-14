53f2a26708f59a0e296803a2c3c8fb322adef48b
karunkop <karunkop@gmail.com>
2025-07-14 22:45:35 +0545

    increased number of waiting frames

--- src/e2e/puppeteer/__tests__/url.ts
+++ src/e2e/puppeteer/__tests__/url.ts
@@ -66,7 +66,7 @@ describe('multiline', () => {
 
     await press('ArrowUp')
 
-    await waitForFrames(4)
+    await waitForFrames(5)
 
     const image = await screenshot()
     expect(image).toMatchImageSnapshot({
