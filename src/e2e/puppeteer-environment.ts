e850fb89efd05fcf2810a8f1caef493f4c4222f0
karunkop <karunkop@gmail.com>
2025-07-16 12:42:10 +0545

    added puppeteer args

--- src/e2e/puppeteer-environment.ts
+++ src/e2e/puppeteer-environment.ts
@@ -1,6 +1,5 @@
 import puppeteer from 'puppeteer-core'
 import { type Environment, builtinEnvironments, populateGlobal } from 'vitest/environments'
-import useSystemFonts from './puppeteer/helpers/useSystemFonts'
 
 /** Puppeteer Environment for vitest. */
 const PuppeteerEnvironment: Environment = {
@@ -22,6 +21,9 @@ const PuppeteerEnvironment: Environment = {
       '--no-sandbox',
       '--no-zygote',
       '--ignore-certificate-errors',
+      '--enable-font-antialiasing',
+      '--font-render-hinting=none',
+      '--disable-gpu',
     ]
 
     const browser = await puppeteer
@@ -35,17 +37,12 @@ const PuppeteerEnvironment: Environment = {
         throw err
       })
 
-    // Create a page and apply system fonts for consistent test rendering
-    const page = await browser.newPage()
-    await useSystemFonts(page)
-
     const { Window, GlobalWindow } = await import('happy-dom')
     const win = new (GlobalWindow || Window)()
 
     const { keys, originals } = populateGlobal(global, {
       ...win,
       browser,
-      page,
     })
 
     return {
