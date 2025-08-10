ce20e0fd0d0ecb1e6a8860b97bb0a94e9aba619f
karunkop <karunkop@gmail.com>
2025-08-10 19:27:16 +0545

    added snapshot for caret-multiline debugging

--- src/e2e/puppeteer/__tests__/caret-multiline.ts
+++ src/e2e/puppeteer/__tests__/caret-multiline.ts
@@ -1,10 +1,18 @@
+import path from 'path'
+import configureSnapshots from '../configureSnapshots'
 import click from '../helpers/click'
 import getSelection from '../helpers/getSelection'
+import hideHUD from '../helpers/hideHUD'
 import paste from '../helpers/paste'
 import press from '../helpers/press'
+import screenshot from '../helpers/screenshot-with-no-antialiasing'
 import waitForEditable from '../helpers/waitForEditable'
 
-vi.setConfig({ testTimeout: 20000, hookTimeout: 20000 })
+expect.extend({
+  toMatchImageSnapshot: configureSnapshots({ fileName: path.basename(__filename).replace('.ts', '') }),
+})
+
+vi.setConfig({ testTimeout: 60000, hookTimeout: 20000 })
 
 describe('all platforms', () => {
   // test case 1
@@ -240,6 +248,7 @@ describe('all platforms', () => {
 
   // test case 10
   it('on cursorUp, the caret should move from the current cursor to the beginning of the multi-line cursor.', async () => {
+    await hideHUD()
     const multiLineCursor =
       "Beautiful antique furnishings fill this quiet, comfortable flat across from the Acropolis museum. AC works great. It is in an heavily touristic area, but the convenience can't be beat. Highly recommended."
     const importText = `
@@ -255,6 +264,8 @@ describe('all platforms', () => {
     await click(editableNodeHandle)
 
     await press('ArrowUp')
+
+    expect(await screenshot()).toMatchImageSnapshot()
 
     // the focus must be at the beginning of the multi-line cursor after cursor up
     const textContext = await getSelection().focusNode?.textContent
